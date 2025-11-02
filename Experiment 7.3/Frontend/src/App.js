94273ead06559306befeabd4c6c9439d39f9830e
triptiagarwal-05 <triptiagarwal.gzb@gmail.com>
2025-11-02 17:56:49 +0530

    Create App.js

---
+++ Experiment 7.3/Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Real-Time Chat</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
